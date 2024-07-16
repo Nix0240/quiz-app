dffbad48c795d5150307167d2f12b911af9c49bb
Nikhilesh Gurjar <nikhilesh.gurjar@openxcell.com>
2024-07-16 07:00:38 +0530

    Added shuffel logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,37 @@
 import React, { useState } from 'react'
 import QUESIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png'
 
 const Quiz = () => {
 const [userAnswer,setUserAnswers]=useState([])
 
 const activeQuestionIndex=userAnswer.length;
+
+const quizComplete=activeQuestionIndex===QUESIONS.length
 function handleSelectAnswer(selectedAnswer){
     setUserAnswers((prevUserAnswers)=> [...prevUserAnswers,selectedAnswer])
 }
+
+if(quizComplete){
+
+  return (
+    <div id='summary'>
+      <img src={quizCompleteImg} alt='quiz complete image'/>
+      <h2>Quiz completed</h2>
+    </div>
+  )
+
+}
+
+const shuffledAnswer=[...QUESIONS[activeQuestionIndex].answers]
+shuffledAnswer.sort(()=> Math.random()-0.5)
 
   return (
    <div id='quiz'>
      <div id='question'>
         <h2>{QUESIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
-        {QUESIONS[activeQuestionIndex].answers.map((answer)=>
+        {shuffledAnswer.map((answer)=>
         <li key={answer} className='answer'>
             <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
         </li>)}
